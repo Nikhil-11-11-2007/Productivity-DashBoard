853c768e94a322cbaaf17966d69b6566f897d8c6
Nikhil-11-11-2007 <ng364357@gmail.com>
2025-12-23 17:55:55 +0530

    pomodoro timer complete

--- script.js
+++ script.js
@@ -130,62 +130,73 @@ function motivationalQuotes() {
 
 motivationalQuotes()
 
-let timer = document.querySelector(".pomo-timer h1")
-let startBtn = document.querySelector(".pomo-timer .start-timer")
-let pauseBtn = document.querySelector(".pomo-timer .pause-timer")
-let resetBtn = document.querySelector(".pomo-timer .reset-timer")
+function pomodoroTimer() {
+    let timer = document.querySelector(".pomo-timer h1")
+    let startBtn = document.querySelector(".pomo-timer .start-timer")
+    let pauseBtn = document.querySelector(".pomo-timer .pause-timer")
+    let resetBtn = document.querySelector(".pomo-timer .reset-timer")
+    let session = document.querySelector(".session")
 
-let isWorkSession = true
+    let isWorkSession = true
 
-let timerInterval = null
+    let timerInterval = null
 
-let totalSeconds = 25 * 60
+    let totalSeconds = 25 * 60
 
-function updateTimer() {
-    let min = Math.floor(totalSeconds / 60)
-    let sec = totalSeconds % 60
+    function updateTimer() {
+        let min = Math.floor(totalSeconds / 60)
+        let sec = totalSeconds % 60
 
-    timer.innerHTML = `${String(min).padStart("2", 0)}:${String(sec).padStart("2", 0)}`
-}
-
-function startTimer() {
-    clearInterval(timerInterval)
-    if (isWorkSession) {
-        totalSeconds = 25 * 60
-        timerInterval = setInterval(function () {
-            if (totalSeconds > 0) {
-                totalSeconds--
-                updateTimer()
-            } else {
-                isWorkSession = false
-                clearInterval(timerInterval)
-            }
-        }, 1000)
-    } else {
-        totalSeconds = 5 * 60
-        timerInterval = setInterval(function () {
-            if (totalSeconds > 0) {
-                totalSeconds--
-                updateTimer()
-            } else {
-                isWorkSession = true
-                clearInterval(timerInterval)
-            }
-        }, 1000)
+        timer.innerHTML = `${String(min).padStart("2", 0)}:${String(sec).padStart("2", 0)}`
     }
+
+    function startTimer() {
+        clearInterval(timerInterval)
+        if (isWorkSession) {
+            timerInterval = setInterval(function () {
+                if (totalSeconds > 0) {
+                    totalSeconds--
+                    updateTimer()
+                } else {
+                    session.innerHTML = "Break Time"
+                    session.style.color = "var(--sec)"
+                    session.style.backgroundColor = "var(--pre)"
+                    totalSeconds = 5 * 60
+                    isWorkSession = false
+                    clearInterval(timerInterval)
+                    timer.innerHTML = `05:00`
+                }
+            }, 1000)
+        } else {
+            timerInterval = setInterval(function () {
+                if (totalSeconds > 0) {
+                    totalSeconds--
+                    updateTimer()
+                } else {
+                    session.innerHTML = "Work Session"
+                    session.style.color = "var(--pre)"
+                    session.style.backgroundColor = "var(--tri1)"
+                    totalSeconds = 25 * 60
+                    isWorkSession = true
+                    clearInterval(timerInterval)
+                    timer.innerHTML = `25:00`
+                }
+            }, 1000)
+        }
+    }
+
+    function pauseTimer() {
+        clearInterval(timerInterval)
+    }
+
+    function resetTimer() {
+        clearInterval(timerInterval)
+        totalSeconds = 25 * 60
+        updateTimer()
+    }
+
+    startBtn.addEventListener("click", startTimer)
+    pauseBtn.addEventListener("click", pauseTimer)
+    resetBtn.addEventListener("click", resetTimer)
 }
-
-function pauseTimer() {
-    clearInterval(timerInterval)
-}
-
-function resetTimer() {
-    clearInterval(timerInterval)
-    totalSeconds = 25 * 60
-    updateTimer()
-}
-
-startBtn.addEventListener("click", startTimer)
-pauseBtn.addEventListener("click", pauseTimer)
-
-resetBtn.addEventListener("click", resetTimer)
+ pomodoroTimer()
